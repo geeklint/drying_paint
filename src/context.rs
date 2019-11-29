84b1b87c9d3baf77a9a6d8cef227d7700b574e7f
Violet <violet@geeklint.com>
2019-11-29 17:11:26 -0500

    some initial documentation

--- src/context.rs
+++ src/context.rs
@@ -6,6 +6,18 @@ thread_local! {
     static CTX_STACK: RefCell<Vec<*const WatchContext>> = RefCell::new(Vec::new());
 }
 
+/// Most of the functions in this crate require that they are executing in
+/// a context.  The context keeps track of some "global" state which enables
+/// the functionality in this crate.
+///
+/// The following will panic if done outside of a WatchContext:
+///   * Calling WatchContext::update_current() (you can use
+/// WatchContext::update() to update a context from outside itself).
+///   * Mutating a Watched<T> value.
+///   * Calling WatchedEvent::dispatch()
+///   * Calling WatchedMeta::trigger() (the two above are actually just
+/// specific variations on this)
+///   * Creating a Watcher<T>
 pub struct WatchContext {
     front_frame: RefCell<WatchSet>,
     back_frame: RefCell<WatchSet>,
@@ -13,33 +25,13 @@ pub struct WatchContext {
 }
 
 impl WatchContext {
+    /// Create a new WatchContext
     pub fn new() -> Self {
         WatchContext {
             front_frame: RefCell::new(WatchSet::new()),
             back_frame: RefCell::new(WatchSet::new()),
             watching_stack: RefCell::new(Vec::new()),
         }
-    }
-
-    pub fn expect_current<F: FnOnce(&WatchContext)>(func: F, msg: &str) {
-        CTX_STACK.with(|stack| {
-            let borrow = stack.borrow();
-            let ptr = borrow.last().expect(msg);
-            (func)(unsafe { ptr.as_ref().unwrap() });
-        });
-    }
-
-    fn internal_update(&self) {
-        while !self.back_frame.borrow().empty() {
-            self.front_frame.swap(&self.back_frame);
-            self.front_frame.borrow_mut().trigger();
-        }
-    }
-
-    pub fn update_current() {
-        Self::expect_current(|ctx| {
-            ctx.internal_update();
-        }, "WatchContext::update_current() called outside of WatchContext");
     }
 
     pub fn with<F: FnOnce()>(&mut self, func: F) {
@@ -50,21 +42,51 @@ impl WatchContext {
         });
     }
 
-    pub fn bind_watch<F: FnOnce()>(&self, watch: WatchRef, func: F) {
+    pub fn update_current() {
+        Self::expect_current(|ctx| {
+            ctx.internal_update();
+        }, "WatchContext::update_current() called outside of WatchContext");
+    }
+
+    pub fn update(&mut self) {
+        self.with(|| Self::update_current());
+    }
+
+    pub(crate) fn expect_current<F: FnOnce(&WatchContext)>(func: F, msg: &str) {
+        CTX_STACK.with(|stack| {
+            let borrow = stack.borrow();
+            let ptr = borrow.last().expect(msg);
+            (func)(unsafe { ptr.as_ref().unwrap() });
+        });
+    }
+
+    pub(crate) fn try_get_current<F: FnOnce(&WatchContext)>(func: F) {
+        CTX_STACK.with(|stack| {
+            let borrow = stack.borrow();
+            if let Some(ptr) = borrow.last() {
+                (func)(unsafe { ptr.as_ref().unwrap() });
+            }
+        });
+    }
+
+    fn internal_update(&self) {
+        while !self.back_frame.borrow().empty() {
+            self.front_frame.swap(&self.back_frame);
+            self.front_frame.borrow_mut().trigger();
+        }
+    }
+
+    pub(crate) fn bind_watch<F: FnOnce()>(&self, watch: WatchRef, func: F) {
         self.watching_stack.borrow_mut().push(watch);
         (func)();
         self.watching_stack.borrow_mut().pop();
     }
 
-    pub fn current_watch(&self) -> Option<WatchRef> {
+    pub(crate) fn current_watch(&self) -> Option<WatchRef> {
         Some(self.watching_stack.borrow().last()?.clone())
     }
     
-    pub fn add_to_next(&self, set: &mut WatchSet) {
+    pub(crate) fn add_to_next(&self, set: &mut WatchSet) {
         self.back_frame.borrow_mut().add_all(set);
-    }
-
-    pub fn update(&mut self) {
-        self.with(|| Self::update_current());
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,124 @@
+//! The name 'drying_paint' comes from the expression "watching paint dry".
+//! This module provides a system to "watch" some values for changes and run
+//! code whenever they change.
+//!
+//! The typical usage is as follows: you first define a structure to hold
+//! data, including some "watched" data.
+//!
+//! ```
+//! # use drying_paint::*;
+//! # type Hello = Watcher<HelloData>;
+//! #[derive(Default)]
+//! struct HelloData {
+//!     name: Watched<String>,
+//!     greeting: String,
+//! }
+//! # impl WatcherInit for HelloData {
+//! #     fn init(watcher: &mut WatcherMeta<Self>) {
+//! #         watcher.watch(|root| {
+//! #             root.greeting = format!("Hello, {}!", root.name);
+//! #         });
+//! #     }
+//! # }
+//! # fn main() {
+//! #     let mut ctx = WatchContext::new();
+//! #     ctx.with(|| {
+//! #         let mut obj = Hello::new();
+//! #         *obj.data_mut().name = "Rust".to_string();
+//! #         WatchContext::update_current();
+//! #         assert_eq!(obj.data().greeting, "Hello, Rust!");
+//! #     });
+//! # }
+//! ```
+//!
+//! Implementing the trait WatcherInit for that structure gives you an place
+//! to set-up the code that should run when a watched value changes.
+//!
+//! ```
+//! # use drying_paint::*;
+//! # type Hello = Watcher<HelloData>;
+//! # #[derive(Default)]
+//! # struct HelloData {
+//! #     name: Watched<String>,
+//! #     greeting: String,
+//! # }
+//! impl WatcherInit for HelloData {
+//!     fn init(watcher: &mut WatcherMeta<Self>) {
+//!         watcher.watch(|root| {
+//!             root.greeting = format!("Hello, {}!", root.name);
+//!         });
+//!     }
+//! }
+//! # fn main() {
+//! #     let mut ctx = WatchContext::new();
+//! #     ctx.with(|| {
+//! #         let mut obj = Hello::new();
+//! #         *obj.data_mut().name = "Rust".to_string();
+//! #         WatchContext::update_current();
+//! #         assert_eq!(obj.data().greeting, "Hello, Rust!");
+//! #     });
+//! # }
+//! ```
+//!
+//! Normally you need to wrap the data struct in a Watcher, so it's common
+//! to alias the watcher type to cleanup the syntax a bit:
+//! ```
+//! # use drying_paint::*;
+//! type Hello = Watcher<HelloData>;
+//! # #[derive(Default)]
+//! # struct HelloData {
+//! #     name: Watched<String>,
+//! #     greeting: String,
+//! # }
+//! # impl WatcherInit for HelloData {
+//! #     fn init(watcher: &mut WatcherMeta<Self>) {
+//! #         watcher.watch(|root| {
+//! #             root.greeting = format!("Hello, {}!", root.name);
+//! #         });
+//! #     }
+//! # }
+//! # fn main() {
+//! #     let mut ctx = WatchContext::new();
+//! #     ctx.with(|| {
+//! #         let mut obj = Hello::new();
+//! #         *obj.data_mut().name = "Rust".to_string();
+//! #         WatchContext::update_current();
+//! #         assert_eq!(obj.data().greeting, "Hello, Rust!");
+//! #     });
+//! # }
+//! ```
+//! Creating watchers and setting watched data needs to happen within a 
+//! WatchContext. WatchContext::update_current() will cause all the pending
+//! watcher code to run.
+//!
+//! ```
+//! # use drying_paint::*;
+//! # type Hello = Watcher<HelloData>;
+//! # #[derive(Default)]
+//! # struct HelloData {
+//! #     name: Watched<String>,
+//! #     greeting: String,
+//! # }
+//! # impl WatcherInit for HelloData {
+//! #     fn init(watcher: &mut WatcherMeta<Self>) {
+//! #         watcher.watch(|root| {
+//! #             root.greeting = format!("Hello, {}!", root.name);
+//! #         });
+//! #     }
+//! # }
+//! fn main() {
+//!     let mut ctx = WatchContext::new();
+//!     ctx.with(|| {
+//!         let mut obj = Hello::new();
+//!         *obj.data_mut().name = "Rust".to_string();
+//!         WatchContext::update_current();
+//!         assert_eq!(obj.data().greeting, "Hello, Rust!");
+//!     });
+//! }
+//! ```
+
+
+#![warn(missing_docs)]
 
 mod trigger;
 use trigger::{Watch, WatchRef, WatchSet};
@@ -20,6 +141,41 @@ pub use event::{
     WatchedEvent
 };
 
+/// An ergonomic wrapper for binding to an WatchedEvent. This is expected to
+/// be used from within a WatcherInit implementation.
+/// ```
+/// use drying_paint::*;
+///
+/// type EventCounter = Watcher<EventCounterData>;
+///
+/// #[derive(Default)]
+/// struct EventCounterData {
+///     counter: u32,
+///     add: WatchedEvent<u32>,
+/// }
+///
+/// impl WatcherInit for EventCounterData {
+///     fn init(watcher: &mut WatcherMeta<Self>) {
+///         bind_event!(watcher => root, root.add => amount, {
+///             root.counter += amount;
+///         });
+///     }
+/// }
+///
+/// fn main() {
+///     let mut ctx = WatchContext::new();
+///     ctx.with(|| {
+///         let mut item = EventCounter::new();
+///         item.data_mut().add.dispatch(7);
+///         WatchContext::update_current();
+///         assert_eq!(item.data().counter, 7);
+///         item.data_mut().add.dispatch(9);
+///         item.data_mut().add.dispatch(3);
+///         WatchContext::update_current();
+///         assert_eq!(item.data().counter, 19);
+///     });
+/// }
+/// ```
 #[macro_export]
 macro_rules! bind_event {
     ( $watcher:expr => $root:ident ,
@@ -76,34 +232,4 @@ mod tests {
         });
     }
 
-    type EventCounter = Watcher<EventCounterData>;
-
-    #[derive(Default)]
-    struct EventCounterData {
-        counter: u32,
-        add: WatchedEvent<u32>,
-    }
-
-    impl WatcherInit for EventCounterData {
-        fn init(watcher: &mut WatcherMeta<Self>) {
-            bind_event!(watcher => root, root.add => amount, {
-                root.counter += amount;
-            });
-        }
-    }
-
-    #[test]
-    fn test_event() {
-        let mut ctx = WatchContext::new();
-        ctx.with(|| {
-            let mut item = EventCounter::new();
-            item.data_mut().add.dispatch(7);
-            WatchContext::update_current();
-            assert_eq!(item.data().counter, 7);
-            item.data_mut().add.dispatch(9);
-            item.data_mut().add.dispatch(3);
-            WatchContext::update_current();
-            assert_eq!(item.data().counter, 19);
-        });
-    }
 }
