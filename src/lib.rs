93d1df008ddd7b10436a514e21836285e76f71ff
Violet <violet@geeklint.com>
2019-11-30 17:28:39 -0500

    add licensing info

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 //! The name 'drying_paint' comes from the expression "watching paint dry".
 //! This module provides a system to "watch" some values for changes and run
 //! code whenever they change.
