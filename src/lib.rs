991ec4adc41b85e3832eb6f442efbbbd926d0b9c
Violet <violet@geeklint.com>
2021-06-19 12:22:40 -0400

    relicense to Apache-2.0 OR MIT OR Zlib

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+/* SPDX-License-Identifier: (Apache-2.0 OR MIT OR Zlib) */
+/* Copyright © 2021 Violet Leonard */
 
 //! The name 'drying_paint' comes from the expression "watching paint dry".
 //! This module provides a system to "watch" some values for changes and run
